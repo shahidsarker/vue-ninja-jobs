9cde4f432740ab9880f2138470cc4db76590002a
Shahid Sarker <shahid.sarker@gmail.com>
2022-01-21 10:32:17 -0500

    loading data with fetch api

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,16 +1,28 @@
 <template>
-  <h1>Job Details Page</h1>
-  The job id is {{ id }}
+  <div v-if="job">
+    <h1>{{ job.title }}</h1>
+    The job id is {{ id }}
+    <p>{{ job.details }}</p>
+  </div>
+  <div v-else>
+    <p>Loading job details...</p>
+  </div>
 </template>
 
 <script>
 export default {
   props: ["id"],
-  //   data() {
-  //     return {
-  //       id: this.$route.params.id,
-  //     };
-  //   },
+  data() {
+    return {
+      job: null,
+    };
+  },
+  mounted() {
+    fetch("http://localhost:3000/jobs/" + this.id)
+      .then((res) => res.json())
+      .then((data) => (this.job = data))
+      .catch((err) => console.log(err));
+  },
 };
 </script>
 
--- src/views/jobs/Jobs.vue
+++ src/views/jobs/Jobs.vue
@@ -1,37 +1,27 @@
 <template>
   <h1>Jobs</h1>
-  <div v-for="job in jobs" :key="job.id" class="job">
-    <router-link :to="{ name: 'JobDetails', params: { id: job.id } }">
-      <h2>{{ job.title }}</h2>
-    </router-link>
+  <div v-if="jobs.length">
+    <div v-for="job in jobs" :key="job.id" class="job">
+      <router-link :to="{ name: 'JobDetails', params: { id: job.id } }">
+        <h2>{{ job.title }}</h2>
+      </router-link>
+    </div>
   </div>
+  <div v-else><p>Loading jobs...</p></div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      jobs: [
-        {
-          title: "Ninja UX Designer",
-          id: 1,
-          details:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, iusto?",
-        },
-        {
-          title: "Ninja Web Developer",
-          id: 2,
-          details:
-            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque molestias consequuntur dignissimos obcaecati! Labore.",
-        },
-        {
-          title: "Ninja Vue Developer",
-          id: 3,
-          details:
-            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo.",
-        },
-      ],
+      jobs: [],
     };
+  },
+  mounted() {
+    fetch("http://localhost:3000/jobs")
+      .then((res) => res.json())
+      .then((data) => (this.jobs = data))
+      .catch((err) => console.log(err));
   },
 };
 </script>
